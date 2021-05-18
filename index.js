50bf2b8fc4e1f1b56117cb0e2804d07eaf0b5333
Dat Dinhquoc <40449395+datdinhquoc@users.noreply.github.com>
2021-05-18 14:22:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function gdrive_update_file(File_Id,File_Name,Binobj){
 }
 
 // Get a file from Gdrive, text only
-log(0999)
+log(03)
 async function gdrive_get_file(File_Id){
     var Metadata = {};
 
@@ -139,6 +139,7 @@ async function gdrive_get_file(File_Id){
     Xhr.responseType = "json";
     Xhr.onload = ()=>{
         log(Xhr)
+        log("t",Xhr.responseText)
         log("b",Xhr.getBody)
         log(Xhr.response)
         log("[Dad's TE] Gdrive file id:",Xhr.response.id); // Retrieve uploaded file ID.
